cc71bed3a2d1545c403e141aab5919071526db11
Luis Enrique <luiskike2210@gmail.com>
2025-03-30 10:29:54 -0600

    Book property 2

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from .forms import PropertyForm
 from .models import Property,Reservation
-from .serializers import PropertiesDetailSerializer, PropertiesListSerializer
+from .serializers import PropertiesDetailSerializer, PropertiesListSerializer, ReservationsListSerializer
 
 @api_view(['GET'])
 @authentication_classes([])
@@ -24,6 +24,16 @@ def properties_detail(request, pk):
     serializer = PropertiesDetailSerializer(property, many=False)
 
     return JsonResponse(serializer.data)
+
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def property_reservations(request, pk):
+    property = Property.objects.get(pk=pk)
+    reservations = property.reservations.all()
+    serializer = ReservationsListSerializer(reservations, many=True)
+    return JsonResponse(serializer.data, safe=False)
 
 @api_view(['POST', 'FILES'])
 def create_property(request):
