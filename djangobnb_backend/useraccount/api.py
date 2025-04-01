188b642b7d12b9e417d57e82b4405ba8b8d24adc
Luis Enrique <luiskike2210@gmail.com>
2025-03-31 18:03:46 -0600

    My reservations dynamic

--- djangobnb_backend/useraccount/api.py
+++ djangobnb_backend/useraccount/api.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from .models import User
 from .serializers import UserDetailSerializer
+from property.serializers import ReservationsListSerializer
 
 @api_view(['GET'])
 @authentication_classes([])
@@ -11,3 +12,13 @@ def landlord_detail(request, pk):
     serializer = UserDetailSerializer(user,many=False)
 
     return JsonResponse(serializer.data,safe=False)
+
+@api_view(['GET'])
+def reservations_list(request):
+    reservations = request.user.reservations.all();
+
+    print('user', request.user)
+    print(reservations)
+    
+    serializer = ReservationsListSerializer(reservations, many=True)
+    return JsonResponse(serializer.data, safe=False)
