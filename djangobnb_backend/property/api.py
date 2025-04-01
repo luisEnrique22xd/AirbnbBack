e03b7b744c015e56a3b167ae33654b06666044b5
Luis Enrique <luiskike2210@gmail.com>
2025-03-31 21:20:10 -0600

    Make it possible to set property as favorite

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -1,21 +1,39 @@
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework_simplejwt.tokens import AccessToken
 from .forms import PropertyForm
 from .models import Property,Reservation
 from .serializers import PropertiesDetailSerializer, PropertiesListSerializer, ReservationsListSerializer
+from useraccount.models import User
 
 @api_view(['GET'])
 @authentication_classes([])
 @permission_classes([])
-
 def properties_list(request):
+
+    try:
+        token = request.META['HTTP_AUTHORIZATION'].split('Bearer ')[1]
+        token = AccessToken(token)
+        user_id = token.payload['user_id']
+        user = User.objects.get(pk=user_id)
+    except Exception as e:
+        user = None
+
+    favorites = []
     properties = Property.objects.all()
     landlord_id= request.GET.get('landlord_id','')
     if landlord_id:
         properties = properties.filter(landlord_id=landlord_id)
+
+    if user:
+        for property in properties:
+            if user in property.favorited.all():
+                favorites.append(property.id)
+
+    print('data:',favorites)
     serializer = PropertiesListSerializer(properties, many=True)
-    return JsonResponse({'data': serializer.data})
+    return JsonResponse({'data': serializer.data, 'favorites':favorites})
 
 
 @api_view(['GET'])
@@ -78,3 +96,16 @@ def book_property(request, pk):
         print('Error', e)
 
         return JsonResponse({'success': False})
+
+@api_view(['POST'])
+def toggle_favorite(request, pk):
+    property = Property.objects.get(pk=pk)
+
+    if request.user in property.favorited.all():
+        property.favorited.remove(request.user)
+
+        return JsonResponse({'is_favorite': False})
+    else:
+        property.favorited.add(request.user)
+
+        return JsonResponse({'is_favorite': True})
