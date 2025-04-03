e325ce00dab518f5c8a342adfa6ddcd5c77f6d23
Luis Enrique <luiskike2210@gmail.com>
2025-04-03 10:11:07 -0600

    Make it possible to connect

--- djangobnb_backend/chat/api.py
+++ djangobnb_backend/chat/api.py
@@ -1,10 +1,22 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from .models import Conversation
-from .serializers import ConversationListSerializer
+from .serializers import ConversationListSerializer, ConversationDetailSerializer
 
 @api_view(['GET'])
 def conversations_list(request):
     serializer = ConversationListSerializer(request.user.conversations.all(), many=True)
 
     return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(['GET'])
+def conversations_detail(request, pk):
+    conversation = request.user.conversations.get(pk=pk)
+
+    conversation_serializer = ConversationDetailSerializer(conversation, many=False)
+
+    return JsonResponse({
+        'conversation': conversation_serializer.data,
+        
+    }, safe=False)
