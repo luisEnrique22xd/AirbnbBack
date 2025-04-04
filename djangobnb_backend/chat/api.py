2ffc5a8aea55acb75b06dca85929dd7e1c4a15ca
Luis Enrique <luiskike2210@gmail.com>
2025-04-03 19:46:42 -0600

    Start new conversation

--- djangobnb_backend/chat/api.py
+++ djangobnb_backend/chat/api.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from .models import Conversation, ConversationMessage
 from .serializers import ConversationListSerializer, ConversationDetailSerializer, ConversationMessageSerializer
-
+from useraccount.models import User
 @api_view(['GET'])
 def conversations_list(request):
     serializer = ConversationListSerializer(request.user.conversations.all(), many=True)
@@ -22,3 +22,19 @@ def conversations_detail(request, pk):
         'messages':message_serializer.data,
         
     }, safe=False)
+
+@api_view(['GET'])
+def conversations_start(request, user_id):
+    conversations = Conversation.objects.filter(users__in=[user_id]).filter(users__in=[request.user.id])
+
+    if conversations.count() > 0:
+        conversation = conversations.first()
+        
+        return JsonResponse({'success': True, 'conversation_id': conversation.id})
+    else:
+        user = User.objects.get(pk=user_id)
+        conversation = Conversation.objects.create()
+        conversation.users.add(request.user)
+        conversation.users.add(user)
+
+        return JsonResponse({'success': True, 'conversation_id': conversation.id})
