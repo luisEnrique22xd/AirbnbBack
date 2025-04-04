bc9b03708368b443dc64fe5c583e6f5a6c9a43b9
Luis Enrique <luiskike2210@gmail.com>
2025-04-03 19:07:09 -0600

    Send/receive messages

--- djangobnb_backend/chat/consumers.py
+++ djangobnb_backend/chat/consumers.py
@@ -7,11 +7,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
 
-        ##join room
+        # Join room
+
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
+
         await self.accept()
 
     async def disconnect(self):
@@ -21,3 +23,33 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+
+    # Recieve message from web sockets
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+
+        conversation_id = data['data']['conversation_id']
+        sent_to_id = data['data']['sent_to_id']
+        name = data['data']['name']
+        body = data['data']['body']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'body': body,
+                'name': name
+            }
+        )
+
+       
+    
+    # Sending messages
+    async def chat_message(self, event):
+        body = event['body']
+        name = event['name']
+
+        await self.send(text_data=json.dumps({
+            'body': body,
+            'name': name
+        }))
